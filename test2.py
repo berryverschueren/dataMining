d39463fd2047c4cacf134c158430f1c44dc586c9
berryverschueren <berryverschueren@gmail.com>
2019-10-21 11:41:33 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -15,7 +15,6 @@ def load_csv(filename):
             dataset.append(row)
     return dataset
 
-#testing colab yeah!
 
 # Initialize a network
 def initialize_network(n_inputs, n_hidden, n_outputs):
